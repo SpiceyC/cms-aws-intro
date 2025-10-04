a2d5a6b7ee01a087c8dfd149ba78ae05bae067c4
Chloe Spicer <chloe.spicer@youview.com>
2025-10-04 12:20:36 +0100

    vpc: add security groups

--- vpc/main/main.tf
+++ vpc/main/main.tf
@@ -18,6 +18,8 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
+########## subnets
+
 resource "aws_subnet" "main_subnet_a" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
@@ -37,3 +39,63 @@ resource "aws_subnet" "main_subnet_b" {
     Subnet = "b"
   }
 }
+
+########## security groups - local
+
+resource "aws_security_group" "allow_local" {
+  name        = "allow_local"
+  description = "Allow local inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "allow_local"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_local_ingress" {
+  security_group_id = aws_security_group.allow_local.id
+  cidr_ipv4         = "86.141.236.221/32"
+  from_port         = 5432
+  ip_protocol       = "tcp"
+  to_port           = 5432
+
+  tags = {
+    Name = "allow_local_ingress"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_egress" {
+  security_group_id = aws_security_group.allow_local.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+
+  tags = {
+    Name = "allow_local_egress"
+  }
+}
+
+######### security group - allow tls
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow tls inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ingress" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.main_vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+
+  tags = {
+    Name = "allow_tls_ingress"
+  }
+}
+
+
--- vpc/main/outputs.tf
+++ vpc/main/outputs.tf
@@ -9,3 +9,11 @@ output "subnet_a_id" {
 output "subnet_b_id" {
   value = aws_subnet.main_subnet_b.id
 }
+
+output "allow_local_sg_id" {
+  value = aws_security_group.allow_local.id
+}
+
+output "allow_tls_sg_id" {
+  value = aws_security_group.allow_tls.id
+}
